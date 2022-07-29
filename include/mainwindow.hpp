e8882c83a578f5f792a8118889754896d05981e0
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-07-30 00:30:55 +0300

    move some logic outside of MainWindow; update: UI

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -6,9 +6,10 @@
 #include <QTimer>
 #include <QMenu>
 
-#include "nvmlpp/nvmlpp_device.hpp"
-
 #include "settings_manager.hpp"
+#include "nvmlpp/nvmlpp_device.hpp"
+#include "gpu_utilizations_controller.hpp"
+#include "gpu_power_controller.hpp"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,7 +27,13 @@ public:
 private slots:
     void toggle_tray();
     void apply_settings(const QJsonObject& settings);
-    void update_dynamic_info();
+
+    void on_GpuUtilizationsController_gpu_utilization(unsigned gpu_utilization);
+    void on_GpuUtilizationsController_memory_utilization(unsigned memory_utilization, unsigned used_memory);
+    void on_GpuUtilizationsController_encoder_decoder_utilization(unsigned encoder_utilization, unsigned decoder_utilization);
+
+    void on_GpuPowerController_power_usage(unsigned power_usage);
+    void on_GpuPowerController_power_limit(unsigned power_limit);
 
     void on_comboBox_select_GPU_activated(int index);
 
@@ -35,6 +42,9 @@ private:
     QSystemTrayIcon tray_icon_;
     SettingsManager& settings_manager_;
 
+    GpuUtilizationsContoller gpu_utilizations_controller_;
+    GpuPowerController gpu_power_controller_;
+
     QTimer dynamic_info_update_timer_;
     bool minimize_to_tray_on_close_;
     int update_freq_ms_;
@@ -42,12 +52,12 @@ private:
     std::vector<NVMLpp::NVML_device> nvml_devices_list_;
 
 protected:
-    void connect_slots();
+    void connect_slots_and_signals();
     void setup_tray_menu();
     void load_app_settings();
     void set_static_info();
     void load_GPUs();    
-    NVMLpp::NVML_device& get_current_gpu();
+    NVMLpp::NVML_device* get_current_gpu();
 
     void closeEvent(QCloseEvent* event);
 };
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -11,6 +11,8 @@ MainWindow::MainWindow(QWidget* parent)
     , ui {new Ui::MainWindow}
     , tray_icon_ {this}
     , settings_manager_ {SettingsManager::instance()}
+    , gpu_utilizations_controller_ {}
+    , gpu_power_controller_ {}
     , dynamic_info_update_timer_ {}
     , minimize_to_tray_on_close_ {false}
     , update_freq_ms_ {}
@@ -19,13 +21,14 @@ MainWindow::MainWindow(QWidget* parent)
     ui->setupUi(this);
     setMinimumSize(size());
 
-    connect_slots();
+    connect_slots_and_signals();
     setup_tray_menu();
     load_app_settings();
     load_GPUs();
 
     set_static_info();
-    update_dynamic_info();
+    gpu_utilizations_controller_.update_info();
+    gpu_power_controller_.update_info();
 
     dynamic_info_update_timer_.start();
 }
@@ -56,12 +59,47 @@ void MainWindow::apply_settings(const QJsonObject& settings)
     qDebug().noquote().nospace() << "new settings applied: " << settings;
 }
 
-void MainWindow::connect_slots()
+void MainWindow::on_GpuUtilizationsController_gpu_utilization(unsigned gpu_utilization)
+{
+    ui->progressBar_GPU_usage->setValue(gpu_utilization);
+}
+
+void MainWindow::on_GpuUtilizationsController_memory_utilization(unsigned memory_utilization, unsigned used_memory)
+{
+    ui->progressBar_GPU_mem_usage->setValue(memory_utilization);
+    ui->lineEdit_GPU_mem_usage->setText(QString::number(used_memory) + " Mib");
+}
+
+void MainWindow::on_GpuUtilizationsController_encoder_decoder_utilization(unsigned encoder_utilization, unsigned decoder_utilization)
+{
+    ui->progressBar_GPU_encoder_usage->setValue(encoder_utilization);
+    ui->progressBar_GPU_decoder_usage->setValue(decoder_utilization);
+}
+
+void MainWindow::on_GpuPowerController_power_usage(unsigned power_usage)
+{
+}
+
+void MainWindow::on_GpuPowerController_power_limit(unsigned power_limit)
+{
+}
+
+void MainWindow::connect_slots_and_signals()
 {
     connect(&tray_icon_, &QSystemTrayIcon::activated, this, &MainWindow::toggle_tray);
-    connect(&dynamic_info_update_timer_, &QTimer::timeout, this, &MainWindow::update_dynamic_info);
-    connect(&settings_manager_, &SettingsManager::error, this,
-            [this](const QString& err_msg)
+
+    connect(&gpu_utilizations_controller_, &GpuUtilizationsContoller::gpu_utilization, this, &MainWindow::on_GpuUtilizationsController_gpu_utilization);
+    connect(&gpu_utilizations_controller_, &GpuUtilizationsContoller::memory_utilization, this, &MainWindow::on_GpuUtilizationsController_memory_utilization);
+    connect(&gpu_utilizations_controller_, &GpuUtilizationsContoller::encoder_decoder_utilization, this, &MainWindow::on_GpuUtilizationsController_encoder_decoder_utilization);
+    connect(&gpu_power_controller_, &GpuPowerController::power_usage, this, &MainWindow::on_GpuPowerController_power_usage);
+    connect(&gpu_power_controller_, &GpuPowerController::power_limit, this, &MainWindow::on_GpuPowerController_power_limit);
+
+    connect(&dynamic_info_update_timer_, &QTimer::timeout, this, [this]()
+    {
+        gpu_utilizations_controller_.update_info();
+        gpu_power_controller_.update_info();
+    });
+    connect(&settings_manager_, &SettingsManager::error, this, [this](const QString& err_msg)
     {
         qCritical().nospace().noquote() << err_msg;
         QMessageBox::critical(this, "Error", err_msg);
@@ -73,7 +111,7 @@ void MainWindow::setup_tray_menu()
 {
     QMenu* tray_menu {new QMenu {this}};
     tray_menu->addAction("Show/Hide app window", this, &MainWindow::toggle_tray);
-    tray_menu->addAction("Quit", this, [this]
+    tray_menu->addAction("Quit", this, [this]()
     {
         minimize_to_tray_on_close_ = false;
         close();
@@ -100,25 +138,14 @@ void MainWindow::load_app_settings()
 void MainWindow::set_static_info()
 {
     const auto& current_gpu {get_current_gpu()};
-    ui->lineEdit_GPU_name->setText(QString::fromStdString(current_gpu.get_name()));
-    ui->lineEdit_GPU_arch->setText(QString::fromStdString(current_gpu.get_arch()));
-    ui->lineEdit_GPU_uuid->setText(QString::fromStdString(current_gpu.get_uuid()));
-    ui->lineEdit_GPU_VBIOS_ver->setText(QString::fromStdString(current_gpu.get_vbios_version()));
+    ui->lineEdit_GPU_name->setText(QString::fromStdString(current_gpu->get_name()));
+    ui->lineEdit_GPU_arch->setText(QString::fromStdString(current_gpu->get_arch()));
+    ui->lineEdit_GPU_uuid->setText(QString::fromStdString(current_gpu->get_uuid()));
+    ui->lineEdit_GPU_VBIOS_ver->setText(QString::fromStdString(current_gpu->get_vbios_version()));
     ui->lineEdit_GPU_driver_ver->setText(QString::fromStdString(NVMLpp::Session::instance().get_system_driver_version()));
-    ui->lineEdit_GPU_total_mem->setText(QString::number(current_gpu.get_total_memory()) + " MiB");
+    ui->lineEdit_GPU_total_mem->setText(QString::number(current_gpu->get_total_memory()) + " MiB");
 
     qDebug().noquote().nospace() << "Static info has been set";
-}
-
-void MainWindow::update_dynamic_info()
-{
-    const auto& current_gpu {get_current_gpu()};
-
-    ui->progressBar_GPU_usage->setValue(current_gpu.get_gpu_utilization());
-    ui->progressBar_GPU_mem_usage->setValue(current_gpu.get_memory_utilization());
-    ui->lineEdit_GPU_mem_usage->setText(QString::number(current_gpu.get_used_memory()) + " MiB");
-    ui->progressBar_GPU_decoder_usage->setValue(current_gpu.get_decoder_utilization());
-    ui->progressBar_GPU_encoder_usage->setValue(current_gpu.get_encoder_utilization());
 }
 
 void MainWindow::load_GPUs()
@@ -129,13 +156,17 @@ void MainWindow::load_GPUs()
         ui->comboBox_select_GPU->addItem(QString::fromStdString(gpu.get_name()));
     }
 
+    const auto& current_gpu {get_current_gpu()};
+    gpu_utilizations_controller_.set_device(current_gpu);
+    gpu_power_controller_.set_device(current_gpu);
+
     qDebug().noquote().nospace() << "Total GPUs found and loaded: " << nvml_devices_list_.size();
 }
 
-NVMLpp::NVML_device& MainWindow::get_current_gpu()
+NVMLpp::NVML_device* MainWindow::get_current_gpu()
 {
     const int current_device_index {ui->comboBox_select_GPU->currentIndex()};
-    return nvml_devices_list_[current_device_index];
+    return &nvml_devices_list_[current_device_index];
 }
 
 void MainWindow::closeEvent(QCloseEvent* event)
@@ -159,5 +190,8 @@ void MainWindow::closeEvent(QCloseEvent* event)
 
 void MainWindow::on_comboBox_select_GPU_activated(int index)
 {
+    const auto& current_gpu {get_current_gpu()};
+    gpu_utilizations_controller_.set_device(current_gpu);
+    gpu_power_controller_.set_device(current_gpu);
     qDebug().noquote().nospace() << "GPU selected: " << ui->comboBox_select_GPU->currentText();
 }
