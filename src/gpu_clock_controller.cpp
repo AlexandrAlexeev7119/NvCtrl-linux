2fc74909ae0c6f3bdc56ef77ea4bcadc32b1fe1a
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-09-15 21:35:39 +0300

    some fixes

--- src/gpu_clock_controller.cpp
+++ src/gpu_clock_controller.cpp
@@ -9,13 +9,14 @@ GpuClockController::GpuClockController(const NVMLpp::NVML_device* nvml_device, Q
 
 void GpuClockController::update_info()
 {
-    clock_values clock_values_;
     try
     {
-        clock_values_.graphics = current_gpu_->get_clock_graphics();
-        clock_values_.video = current_gpu_->get_clock_video();
-        clock_values_.sm = current_gpu_->get_clock_sm();
-        clock_values_.mem = current_gpu_->get_clock_memory();
+        clock_values clock_values_ {
+            .graphics = current_gpu_->get_clock_graphics(),
+            .video = current_gpu_->get_clock_video(),
+            .sm = current_gpu_->get_clock_sm(),
+            .mem = current_gpu_->get_clock_memory()
+        };
         emit info_ready(clock_values_);
     }
     catch (const NVMLpp::errors::error&)
