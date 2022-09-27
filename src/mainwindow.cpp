4209e0d8fbdf4e0b8c86cd82dd3d3c594b1fbba3
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-09-27 22:49:41 +0300

    added some new info, UI changes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -237,6 +237,7 @@ void MainWindow::set_static_info()
     ui->lineEdit_GPU_name->setText(QString::fromStdString(current_gpu->get_name()));
     ui->lineEdit_GPU_arch->setText(QString::fromStdString(current_gpu->get_arch()));
     ui->lineEdit_GPU_vendor->setText(QString::fromStdString(current_gpu->get_vendor()));
+    ui->lineEdit_GPU_subvendor->setText(QString::fromStdString(current_gpu->get_subvendor()));
     ui->lineEdit_GPU_VBIOS_ver->setText(QString::fromStdString(current_gpu->get_vbios_version()));
     ui->lineEdit_GPU_driver_ver->setText(QString::fromStdString(NVMLpp::Session::instance().get_system_driver_version()));
     ui->lineEdit_GPU_bus_type->setText(QString::fromStdString(current_gpu->get_bus_type()));
