f5e3e30dd7218a3db807fd885373c7f086652be9
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-10-22 20:27:58 +0300

    fix build type and version

--- include/app_config.hpp
+++ include/app_config.hpp
@@ -3,10 +3,10 @@
 namespace GWEpp::config
 {
 #ifdef NDEBUG
-constexpr const char* APP_VERSION_STRING {APP_VERSION "-Release build"};
+constexpr const char* APP_VERSION_STRING {APP_VERSION " (Release build)"};
 constexpr bool IS_DEBUG_BUILD {false};
 #else
-constexpr const char* APP_VERSION_STRING {APP_VERSION "-Debug build"};
+constexpr const char* APP_VERSION_STRING {APP_VERSION " (Debug build)"};
 constexpr bool IS_DEBUG_BUILD {true};
 #endif
 } // namespace GWEpp::config
