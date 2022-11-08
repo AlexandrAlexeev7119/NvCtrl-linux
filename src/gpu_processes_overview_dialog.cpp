fb34a750e768d8bb9278f3c075deca089a731c83
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-11-08 23:23:20 +0300

    fixed: wrong mem usage for processes

--- src/gpu_processes_overview_dialog.cpp
+++ src/gpu_processes_overview_dialog.cpp
@@ -52,33 +52,29 @@ void GpuProcessesOverviewDialog::on_buttonBox_rejected()
 
 void GpuProcessesOverviewDialog::show_processes_info()
 {
-    using vec_size_t = std::vector<NVML_native::nvmlProcessInfo_t>::size_type;
     const auto proc_list {current_gpu_->get_running_processes()};
 
     qDebug().noquote().nospace() << "Processes run on GPU: " << proc_list.size();
 
-    if (static_cast<vec_size_t>(ui->tableWidget_proc_info->rowCount()) != proc_list.size())
+    ui->tableWidget_proc_info->clearContents();
+    ui->tableWidget_proc_info->setRowCount(proc_list.size());
+
+    int curr_row {0};
+    std::for_each(proc_list.begin(), proc_list.end(), [this, &curr_row](const NVML_native::nvmlProcessInfo_t& process)
     {
-        ui->tableWidget_proc_info->clearContents();
-        ui->tableWidget_proc_info->setRowCount(proc_list.size());
+        ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_PID, new QLabel {
+                                                     QString::number(process.pid), ui->tableWidget_proc_info
+                                                 });
+        ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_MEM_USAGE, new QLabel {
+                                                     QString{"%1 MiB"}.arg(process.usedGpuMemory / 1024 / 1024), ui->tableWidget_proc_info
+                                                 });
+        ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_NAME, new QLabel {
+                                                     QString::fromStdString(get_process_name_by_pid(process.pid)), ui->tableWidget_proc_info
+                                                 });
+        curr_row++;
+    });
 
-        int curr_row {0};
-        std::for_each(proc_list.begin(), proc_list.end(), [this, &curr_row](const NVML_native::nvmlProcessInfo_t& process)
-        {
-            ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_PID, new QLabel {
-                                                         QString::number(process.pid), ui->tableWidget_proc_info
-                                                     });
-            ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_MEM_USAGE, new QLabel {
-                                                         QString{"%1 MiB"}.arg(process.usedGpuMemory / 1024 / 1024), ui->tableWidget_proc_info
-                                                     });
-            ui->tableWidget_proc_info->setCellWidget(curr_row, CELL_PROC_NAME, new QLabel {
-                                                         QString::fromStdString(get_process_name_by_pid(process.pid)), ui->tableWidget_proc_info
-                                                     });
-            curr_row++;
-        });
-
-        qDebug().noquote().nospace() << "Processes list changed, widgets updated";
-    }
+    qDebug().noquote().nospace() << "Processes list changed, widgets updated";
 }
 
 
