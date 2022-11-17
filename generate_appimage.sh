07008bdaf1a01233392007dbe99711eae36e5901
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-11-17 16:52:41 +0300

    update AppImage build script

--- generate_appimage.sh
+++ generate_appimage.sh
@@ -2,13 +2,13 @@
 
 CURR_DIR=`pwd`
 APP_VER="v$(cat ./.last_version)"
-APP_DIR="./GWEpp.AppDir"
-BIN_DIR="./build-GWEpp-GCC-Release"
+APP_DIR="./NvCtrl-Linux.AppDir"
+BIN_DIR="./build-NvCtrl-Linux-GCC-Release"
 
 cd $BIN_DIR && cmake .. && make clean && make -j$(nproc)
 cd $CURR_DIR
-cp $BIN_DIR/gwepp $APP_DIR/usr/bin/.
+cp $BIN_DIR/NvCtrl-Linux $APP_DIR/usr/bin/.
 
 appimagetool $APP_DIR
-mv GWEpp-x86_64.AppImage GWEpp-$APP_VER-x86_64.AppImage
+mv NvCtrl-Linux-x86_64.AppImage NvCtrl-Linux-$APP_VER-x86_64.AppImage
 
