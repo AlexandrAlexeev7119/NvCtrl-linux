47e36dfa5a1585e38d67df2f97149354a3d5758f
AlexCr4ckPentest <alexcr4ckpentest@pc-h110m-s2pv.localdomain>
2022-12-02 12:33:33 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+### NvCtrl-linux build script
+
+function do_build()
+{
+  if [[ ! -d build ]]
+  then
+    mkdir build
+  fi
+  cd build
+  cmake -DCMAKE_BUILD_TYPE=Release ..
+  make -j$(nproc)
+}
+
+do_build
+
